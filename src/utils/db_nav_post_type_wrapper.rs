ead532e30ccf4dd85526428fedca16c69e37de7e
vendelieu <vennywise>
2022-06-30 08:19:07 +0300

    make NavPost, Tag types more explicit.

--- src/utils/db_nav_post_type_wrapper.rs
+++ src/utils/db_nav_post_type_wrapper.rs
@@ -1,9 +1,10 @@
-use diesel::pg::Pg;
+use std::io::Write;
+
 use diesel::{deserialize, serialize};
 use diesel::deserialize::FromSql;
+use diesel::pg::Pg;
 use diesel::serialize::{Output, ToSql, WriteTuple};
 use diesel::sql_types::{Record, Text};
-use std::io::Write;
 
 #[derive(SqlType)]
 #[postgres(type_name = "nav_post")]
@@ -11,17 +12,20 @@ pub struct NavPostType;
 
 #[derive(Debug, PartialEq, FromSqlRow, AsExpression, Serialize, Deserialize)]
 #[sql_type = "NavPostType"]
-pub struct NavPost(String, String);
+pub struct NavPost {
+    pub title: String,
+    pub slug: String,
+}
 
 impl ToSql<NavPostType, Pg> for NavPost {
     fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
-        WriteTuple::<(Text, Text)>::write_tuple(&(self.0.as_str(), self.1.as_str()), out)
+        WriteTuple::<(Text, Text)>::write_tuple(&(self.title.as_str(), self.slug.as_str()), out)
     }
 }
 
 impl FromSql<NavPostType, Pg> for NavPost {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
         let (title, slug) = FromSql::<Record<(Text, Text)>, Pg>::from_sql(bytes)?;
-        Ok(NavPost(title, slug))
+        Ok(NavPost { title, slug })
     }
 }
--- src/utils/db_tag_type_wrapper.rs
+++ src/utils/db_tag_type_wrapper.rs
@@ -1,9 +1,9 @@
-use diesel::pg::Pg;
+use std::io::Write;
 use diesel::{deserialize, serialize};
 use diesel::deserialize::FromSql;
+use diesel::pg::Pg;
 use diesel::serialize::{Output, ToSql, WriteTuple};
 use diesel::sql_types::{Integer, Record, Text};
-use std::io::Write;
 
 #[derive(SqlType)]
 #[postgres(type_name = "tag")]
@@ -11,17 +11,21 @@ pub struct TagType;
 
 #[derive(Debug, PartialEq, FromSqlRow, AsExpression, Deserialize, Serialize)]
 #[sql_type = "TagType"]
-pub struct Tag(i32, String, String);
+pub struct Tag {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+}
 
 impl ToSql<TagType, Pg> for Tag {
     fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
-        WriteTuple::<(Integer, Text, Text)>::write_tuple(&(self.0, self.1.as_str(), self.2.as_str()), out)
+        WriteTuple::<(Integer, Text, Text)>::write_tuple(&(self.id, self.name.as_str(), self.slug.as_str()), out)
     }
 }
 
 impl FromSql<TagType, Pg> for Tag {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
         let (id, name, slug) = FromSql::<Record<(Integer, Text, Text)>, Pg>::from_sql(bytes)?;
-        Ok(Tag(id, name, slug))
+        Ok(Tag { id, name, slug })
     }
 }
