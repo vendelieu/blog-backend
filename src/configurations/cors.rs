243b41106bf8ad0bb3c198aa263983f130e8b50f
vendelieu <vennywise>
2022-06-30 21:25:33 +0300

    change cors config, allow credentials

--- src/configurations/cors.rs
+++ src/configurations/cors.rs
@@ -1,13 +1,13 @@
 use actix_cors::Cors;
 use actix_web::http;
+use http::header::{ACCEPT, CONTENT_TYPE};
 
 pub fn get_config() -> Cors {
     Cors::default() // allowed_origin return access-control-allow-origin: * by default
         .allowed_origin("http://127.0.0.1:3000")
         .allowed_origin("http://localhost:3000")
-        .send_wildcard()
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-        .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-        .allowed_header(http::header::CONTENT_TYPE)
+        .allowed_headers(vec![ACCEPT, CONTENT_TYPE])
+        .supports_credentials()
         .max_age(3600)
 }
