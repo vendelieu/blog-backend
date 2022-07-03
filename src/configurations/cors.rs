4bf41cbad3a76d2586dcf2d25a462d71ecbf6b38
vendelieu <vennywise>
2022-07-03 04:20:44 +0300

    change cors port to 4200

--- src/configurations/cors.rs
+++ src/configurations/cors.rs
@@ -4,8 +4,8 @@ use http::header::{ACCEPT, CONTENT_TYPE};
 
 pub fn get_config() -> Cors {
     Cors::default() // allowed_origin return access-control-allow-origin: * by default
-        .allowed_origin("http://127.0.0.1:3000")
-        .allowed_origin("http://localhost:3000")
+        .allowed_origin("http://127.0.0.1:4200")
+        .allowed_origin("http://localhost:4200")
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
         .allowed_headers(vec![ACCEPT, CONTENT_TYPE])
         .supports_credentials()
