4740710c830cc11eef0cf27fbe5762e494e45c4b
vendelieu <vennywise>
2022-07-03 07:59:21 +0300

    change rate limits per sec from 5 to 10

--- src/configurations/rate_limiting_governor.rs
+++ src/configurations/rate_limiting_governor.rs
@@ -3,7 +3,7 @@ use actix_governor::{GovernorConfig, GovernorConfigBuilder, PeerIpKeyExtractor};
 pub fn get_governor() -> GovernorConfig<PeerIpKeyExtractor> {
     GovernorConfigBuilder::default()
         .per_second(2)
-        .burst_size(5)
+        .burst_size(10)
         .finish()
         .unwrap()
 }
