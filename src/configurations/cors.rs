c9a53d03637f074e65a9f086641b3c0a71bcb0fa
vendelieu <vennywise>
2022-08-23 02:11:37 +0300

    add port to cors origin

--- src/configurations/cors.rs
+++ src/configurations/cors.rs
@@ -6,7 +6,7 @@ pub fn get_config() -> Cors {
     Cors::default() // allowed_origin return access-control-allow-origin: * by default
         .allowed_origin("http://127.0.0.1:4200")
         .allowed_origin("http://localhost:4200")
-        .allowed_origin("https://vendeli.eu")
+        .allowed_origin("https://vendeli.eu:8080")
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
         .allowed_headers(vec![ACCEPT, CONTENT_TYPE])
         .supports_credentials()
